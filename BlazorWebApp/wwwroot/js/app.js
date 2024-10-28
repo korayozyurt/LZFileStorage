19156ae40f92d3fc30bab132606eab4c51a1015b
koray.ozyurt <koray.ozyurt@outlook.com>
2024-10-28 21:18:46 +0300

    swal

--- BlazorWebApp/wwwroot/js/app.js
+++ BlazorWebApp/wwwroot/js/app.js
@@ -1,13 +1,1 @@
-﻿function SwalLoading() {
-    Swal.showLoading();
-}
-
-function MesajGoster(title, message, status) {
-    $(function () {
-        Swal.fire(title, message, status);
-    });
-}
-
-function SwalClose() {
-    Swal.close();
-}
+﻿
