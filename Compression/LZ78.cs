3d5ba14fd86717e155010e87ca22071a4dea599f
koray.ozyurt <koray.ozyurt@outlook.com>
2024-11-04 23:02:53 +0300

    LZ8 sıkıştırma

--- Compression/LZ78.cs
+++ Compression/LZ78.cs
@@ -1,6 +1,6 @@
 ﻿using System.Text;
 
-namespace LZAlgorithms;
+namespace Compression;
 
 public class LZ78
 {
@@ -73,6 +73,37 @@ public class LZ78
         return compressed;
     }
 
+    public List<TupleByte> Compress(byte[] input)
+    {
+        Dictionary<string, int> dictionary = new Dictionary<string, int>();
+        List<TupleByte> compressed = new List<TupleByte>();
+        string currentString = String.Empty;
+        int dictSize = 1;
+
+        foreach (byte b in input)
+        {
+            string concatenated = currentString + (char)b;
+            if (dictionary.ContainsKey(concatenated))
+            {
+                currentString = concatenated;
+            }
+            else
+            {
+                var index = currentString == string.Empty ? 0 : dictionary[currentString];
+                compressed.Add(new TupleByte(index, b));
+                dictionary[concatenated] = dictSize++;
+                currentString = string.Empty;
+            }
+        }
+        if (currentString != string.Empty)
+        {
+            int index = dictionary[currentString];
+            compressed.Add(new TupleByte(index, 0)); // Sonraki karakter olmadığı için 0 değeri ekliyoruz
+        }
+
+        return compressed;
+    } 
+
     public string Decompress(List<TupleChar> compressed)
     {
         Dictionary<int, string> dictionary = new Dictionary<int, string>();
@@ -90,4 +121,53 @@ public class LZ78
 
         return decompressed.ToString();
     }
+
+    public byte[] Decompress(List<TupleByte> compressed)
+    {
+        Dictionary<int, List<byte>> dictionary = new Dictionary<int, List<byte>>();
+        List<byte> decompressed = new List<byte>();
+        int dictSize = 1;
+
+        foreach (TupleByte item in compressed)
+        {
+            List<byte> entry = item.Index == 0 ? new List<byte>() : dictionary[item.Index];
+            List<byte> newEntry = new List<byte>(entry);
+            newEntry.Add(item.NextByte);
+
+            decompressed.AddRange(newEntry);
+            dictionary[dictSize++] = newEntry;
+        }
+
+        return decompressed.ToArray();
+    }
+
+    public void WriteToFile(string filePath, List<TupleByte> compressed)
+    {
+        using (BinaryWriter writer = new BinaryWriter(File.Open(filePath, FileMode.Create)))
+        {
+            foreach (var tuple in compressed)
+            {
+                writer.Write(tuple.Index);  
+                writer.Write(tuple.NextByte);
+            }
+        }
+    }
+
+    public List<TupleByte> ReadFromFile(string filePath)
+    {
+        List<TupleByte> compressed = new List<TupleByte>();
+
+        using (BinaryReader reader = new BinaryReader(File.Open(filePath, FileMode.Open)))
+        {
+            while (reader.BaseStream.Position != reader.BaseStream.Length)
+            {
+                int index = reader.ReadInt32();
+                byte nextByte = reader.ReadByte();
+                compressed.Add(new TupleByte(index, nextByte));
+            }
+        }
+
+        return compressed;
+    }
+
 }
